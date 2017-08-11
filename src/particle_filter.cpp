3e5d6440c854e5b34584857fba85284bcdd489a3
Ronald Sostaric <ronsos@gmail.com>
2017-08-11 15:25:59 -0500

    Working filter, coverging on wrong particle

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
-    num_particles = 100;
+    num_particles = 10;
     
     // init randon gen
     default_random_engine gen;
@@ -48,10 +48,17 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
         // NOTE: weights may need to be at the pf level, not ind particles
         
         particles.push_back(p);
-     
-        //cout << "Sample " << p.id << " " << p.x << " " << p.y << " " << p.theta << endl;
-        //cout << "Sample " << particles[i].id << endl;
     }
+    
+   
+   // Set init flag to true     
+   is_initialized = true;
+   
+   // Print initial states
+   for(int n=0; n<num_particles; ++n) {
+       cout << "INIT: " << particles[n].id << ", x = " << particles[n].x << ", y =" << particles[n].y << ", theta = " << particles[n].theta << endl;
+   }
+    
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -108,59 +115,69 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
     
-    cout << "size of particles =" << particles.size() << endl;
+    //Declarations
+    float x_p, y_p, theta;
+    float x_map, y_map;
+    float range_closest, x_closest, y_closest, range_k;
+    double Q, MVGaussian, MVG_mult;
     
-    // Find nearest landmark for each observation 
+    // For each particle, find weights  
     for (int i=0; i<particles.size(); i++) {
         
         // Init MVG_mult for weights
-        double MVG_mult = 1;
+        MVG_mult = 1;
         
         // use local variables for particle states for ease of reading
-        float x_p = particles[i].x;
-        float y_p = particles[i].y;
-        float theta = particles[i].theta;
+        x_p = particles[i].x;
+        y_p = particles[i].y;
+        theta = particles[i].theta;
         
         // convert observations to map frame
         // based on lesson 14.13
         for (int j=0; j<observations.size(); j++) {
-            float x_map = observations[j].x + x_p*cos(theta) + y_p*sin(theta);
-            float y_map = observations[j].y - x_p*sin(theta) + y_p*cos(theta);
+           
+            //x_map = observations[j].x + x_p*cos(theta) + y_p*sin(theta);
+            //y_map = observations[j].y - x_p*sin(theta) + y_p*cos(theta);
+            x_map = x_p + observations[j].x*cos(theta) + observations[j].y*sin(theta);
+            y_map = y_p - observations[j].x*sin(theta) + observations[j].y*cos(theta);
+            
+            //cout << "x_map = " << x_map << ", Y_map = " << y_map << endl;
             
             // Initialize closest range parameter with big number
-            float range_closest = 100000000000000.0;
-            float x_closest = 100000000000000.0;
-            float y_closest = 100000000000000.0;
+            range_closest = 100.0;
             
             // find the closest landmark for the observation
             for (int k=0; k<map_landmarks.landmark_list.size(); k++) {    
            
                 // Calculate distance to each landmark
-                float range_k = dist(x_map, y_map, map_landmarks.landmark_list[k].x_f, \
+                range_k = dist(x_map, y_map, map_landmarks.landmark_list[k].x_f, \
                                      map_landmarks.landmark_list[k].y_f); 
+                
                 
                 // if current closest landmark, save 
                 if (range_k < range_closest) {
                     range_closest = range_k;
-                    float x_closest = map_landmarks.landmark_list[k].x_f;
-                    float y_closest = map_landmarks.landmark_list[k].y_f;             
+                    x_closest = map_landmarks.landmark_list[k].x_f;
+                    y_closest = map_landmarks.landmark_list[k].y_f;  
+                  
                 }
             }
+            //cout << "range k =" << range_k << endl;
+            //cout << "range closest =" << range_closest << endl;
                 
             // Use closest landmark to find MVG prob for each observation
-            float Q = -(pow(x_map-x_closest,2.0)/(2*pow(std_landmark[0],2.0)) + \
-                        pow((y_map-y_closest),2.0)/pow(2*std_landmark[1],2.0));
-            float MVGaussian = 1 / (2*M_PI*std_landmark[0]*std_landmark[1]) * exp(Q);
-                
-            // mulitplty all the probabilities together (for all observations) 
-            // to get the total probabilty for the particle
-            MVG_mult *= MVGaussian;                
+            Q = -(pow(x_map-x_closest,2.0)/(2*pow(std_landmark[0],2.0)) + \
+                        pow((y_map-y_closest),2.0)/(2*(pow(std_landmark[1],2.0))));
+            MVGaussian = 1 / (2*M_PI*std_landmark[0]*std_landmark[1]) * exp(Q);
+            MVG_mult *= MVGaussian; 
+            
         }
         
         // update particle weight
-        particles[i].weight = MVG_mult;        
+        particles[i].weight = MVG_mult;  
+        
     }
-    // need to normalize particle weights???
+    
 }
 
 void ParticleFilter::resample() {
@@ -168,15 +185,38 @@ void ParticleFilter::resample() {
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
     
+    // Populate weights for forming distribution
+    std::vector<double> weights;
+    for(int j = 0; j < num_particles; j++){
+        Particle p = particles[j];
+        weights.push_back(p.weight);
+        cout << particles[j].id << ", weight = " << p.weight <<endl;
+    }
+    
+    // Initialize variables and generator
+    std::vector<Particle> particles_new;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::discrete_distribution<> d(weights.begin(), weights.end());
-    //std::discrete_distribution<> d(particles.weight.begin(), particles.weight.end());
-    //std::map<int, int> m;
+    
+    // Resample new set of particles
     for(int n=0; n<num_particles; ++n) {
-        //particles_new.push_back(particles[d(gen)]);
-        particles.push_back(particles[d(gen)]);
+        Particle particle_res = particles[d(gen)];
+        particles_new.push_back(particle_res);    
     }
+    
+    // replace particles with new ones
+    particles = particles_new;
+    //Particle &p = particles[i];
+    
+    // Print updated particle states 
+    for(int n=0; n<num_particles; ++n) {
+        cout << particles[n].id << ", x = " << particles[n].x << ", y = " << particles[n].y << ", theta = " << particles[n].theta << endl;
+    }
+    
+ 
+    
+    
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)
